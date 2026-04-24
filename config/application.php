3ddbe5abf71e26688eee2622297f473c8bb78277
Tiago França <github@tiagofranca.com>
2026-04-23 23:37:45 -0300

    feat: add application configuration file for server IP whitelist management

---
+++ config/application.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'server' => [
+        /**
+         * List of IPs (IP range) used by the application.
+         * Useful for users to release IPs in applications bypass/rules IPs such as Firewalls, EC2 Security Group, etc
+         */
+        'ip_list' => [
+            /**
+             * Ex:
+             * 200.227.0.0/24
+             * 160.0.0.0/12
+             * 160.0.0.0/12
+             * 160.200.0.0/24
+             *
+            */
+            //
+        ],
+    ],
+    //
+];
